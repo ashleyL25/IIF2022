e8db982f9e570eb5a30f403217cee3ba6355417a
ashleyL25 <almcdowell.home@gmail.com>
2022-08-29 11:28:25 -0500

    uploading

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  wordpress: {
+    api_url: 'https://app.iowairishfest.com/wp-json/wp/v2/',
+    auth_url: 'https://app.iowairishfest.com/wp-json/api/v1/token'
+  }
+};
